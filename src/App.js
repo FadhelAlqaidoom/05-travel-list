9a3253c864ce26c6c503ac1e4de3cdc6f5855472
fadhel <fadhelalqaidoom@icloud.com>
2023-10-02 10:22:01 +0300

    create Logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import Logo from './components/Logo';
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
-  );
+  return <Logo />;
 }
 
 export default App;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div className="flex flex-row items-center justify-center bg-orange-400 h-16">
+      <h1 className="text-3xl font-bold">FAR AWAY</h1>
+    </div>
+  );
+};
+
+export default Logo;
